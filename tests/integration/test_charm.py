8f8772172ec786df854a460bc3c927b0431482ac
Raul Zamora <raul.zamora@canonical.com>
2024-09-16 10:57:27 +0200

    add fast forward

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -50,9 +50,10 @@ async def test_integrate_kafka(ops_test: OpsTest):
     await ops_test.model.wait_for_idle(apps=[ZOOKEEPER, KAFKA], idle_period=30, timeout=3600)
 
     await ops_test.model.add_relation(KAFKA, ZOOKEEPER)
-    await ops_test.model.wait_for_idle(
-        apps=[KAFKA, ZOOKEEPER], idle_period=30, timeout=1800, status="active"
-    )
+    async with ops_test.fast_forward(fast_interval="60s"):
+        await ops_test.model.wait_for_idle(
+            apps=[KAFKA, ZOOKEEPER], idle_period=30, timeout=1800, status="active"
+        )
 
     assert ops_test.model.applications[KAFKA].status == "active"
     assert ops_test.model.applications[ZOOKEEPER].status == "active"
