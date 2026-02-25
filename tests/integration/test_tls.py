b7e0a5216eb49ad944e98b22ef449e08ff6b8891
Raúl Zamora Martínez <76525382+zmraul@users.noreply.github.com>
2026-02-25 09:36:37 +0100

    [DPE-8614] Enable Rest TLS (#27)
    * enable tls for rest endpoints
    
    * fix restart and config changed triggers

--- tests/integration/test_tls.py
+++ tests/integration/test_tls.py
@@ -6,14 +6,25 @@ import asyncio
 import logging
 
 import pytest
-from helpers import APP_NAME, KAFKA, KARAPACE_CONTAINER, ZOOKEEPER, set_tls_private_key
+import requests
+from helpers import (
+    APP_NAME,
+    CA_FILE,
+    KAFKA,
+    KARAPACE_CONTAINER,
+    ZOOKEEPER,
+    get_address,
+    get_admin_credentials,
+    set_tls_private_key,
+)
 from pytest_operator.plugin import OpsTest
+
+from literals import PORT
 
 logger = logging.getLogger(__name__)
 
 TLS_NAME = "self-signed-certificates"
 TLS_CHANNEL = "1/stable"
-CERTS_NAME = "tls-certificates-operator"
 
 
 @pytest.mark.abort_on_fail
@@ -36,7 +47,10 @@ async def test_deploy_tls(ops_test: OpsTest, karapace_charm):
         ops_test.model.deploy(TLS_NAME, channel=TLS_CHANNEL, config=tls_config),
     )
     await ops_test.model.wait_for_idle(
-        apps=[APP_NAME, ZOOKEEPER, KAFKA, TLS_NAME], idle_period=30, timeout=1800
+        apps=[APP_NAME, ZOOKEEPER, KAFKA, TLS_NAME],
+        idle_period=30,
+        timeout=1800,
+        raise_on_error=False,
     )
 
     assert ops_test.model.applications[APP_NAME].status == "blocked"
@@ -82,3 +96,35 @@ async def test_karapace_tls(ops_test: OpsTest):
 
     assert ops_test.model.applications[APP_NAME].status == "active"
     assert ops_test.model.applications[KAFKA].status == "active"
+
+
+@pytest.mark.abort_on_fail
+async def test_schema_creation(ops_test: OpsTest):
+    """Check that a schema can be registered using internal credentials."""
+    # Store the CA cert for requests
+    action = await ops_test.model.units.get(f"{TLS_NAME}/0").run_action("get-ca-certificate")
+    ca = await action.wait()
+    ca = ca.results.get("ca-certificate")
+    open(CA_FILE, "w").write(ca)
+
+    schema_name = "test-key"
+    operator_password = await get_admin_credentials(ops_test)
+    address = await get_address(ops_test=ops_test)
+    base_url = f"https://{address}:{PORT}"
+    auth = ("operator", operator_password)
+
+    # Create the schema
+    schema_data = {
+        "schema": '{"type": "record", "name": "Obj", "fields":[{"name": "age", "type": "int"}]}'
+    }
+
+    response = requests.post(
+        f"{base_url}/subjects/{schema_name}/versions",
+        json=schema_data,
+        headers={"Content-Type": "application/vnd.schemaregistry.v1+json"},
+        auth=auth,
+        verify=CA_FILE,
+    )
+    response.raise_for_status()
+    result = response.text
+    assert '{"id":1}' in result
